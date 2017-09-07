5d46c6750f5260781f521091d9ba1acb7c26dc83
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-09-06 20:45:39 -0700

    1. created a new component, created a new service and registered it into the providers array, used the service into the new created component and rendered the list of authors from with in, compolated the assignment

---
+++ src/app/authors/authors.component.ts
@@ -0,0 +1,20 @@
+import { AuthorsService } from './../authors.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+  title = "Authors";
+  authors;
+
+  constructor(authorService: AuthorsService) {
+    this.authors = authorService.getAuthors();
+   }
+  
+  ngOnInit() {
+  }
+
+}
