138585cecb975df3b0cdceddf415005a242fb2b7
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-09-08 20:36:47 -0700

    created a new component, coded the view and binded the data using [(ngModel)], and used the new Pipe to apply the algorithm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
+import { TitleCase } from './title-case.pipe';
 import { AuthorsService } from './authors.service';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { FavoriteIconComponent } from './favorite-icon/favorite-icon.component';
+import { TitleCaseComponent } from './title-case/title-case.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorsComponent,
-    FavoriteIconComponent
+    FavoriteIconComponent,
+    TitleCaseComponent,
+    TitleCase
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [AuthorsService],
   bootstrap: [AppComponent]
